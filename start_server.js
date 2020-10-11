3f7595d662edd1a62614cab653890583fd144454
HfCloud <sxysxygm@gmail.com>
2020-10-12 00:45:33 +0800

    update

--- start_server.js
+++
@@ -1,7 +0,0 @@
-let liveServer = require("live-server");
-let params = { port : 80,
-               host : "0.0.0.0", //地址回环
-               root : "./",
-               open : false
-             };
-liveServer.start(params);
